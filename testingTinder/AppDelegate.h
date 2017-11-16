9eeeb4efa7ae6746c2ed9209769b93dd19a0b321
Nicky Patson <nicky.patson@cloudium.io>
2017-11-16 10:24:15 +0530

    Initial Commit

---
+++ testingTinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testingTinder
+//
+//  Created by Nicky on 11/16/17.
+//  Copyright © 2017 Nicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testingTinder/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testingTinder
+//
+//  Created by Nicky on 11/16/17.
+//  Copyright © 2017 Nicky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
