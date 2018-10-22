81c05d99a98a95451fc73c37503283a9cd4b7e91
Nicky Patson <nicky.patson@cloudium.io>
2018-10-22 10:35:37 +0530

    Added gradient View

---
+++ testingTinderSwipe/GradientView.swift
@@ -0,0 +1,112 @@
+//
+//  GradientView.swift
+//  testingTinderSwipe
+//
+//  Created by Nick on 19/10/18.
+//  Copyright © 2018 Nicky. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class GradientView: UIView {
+    
+    
+    private var gradientLayer: CAGradientLayer!
+    
+    @IBInspectable var topColor: UIColor = .red {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = .yellow {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor = .clear {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var shadowX: CGFloat = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var shadowY: CGFloat = -3 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var shadowBlur: CGFloat = 3 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var startPointX: CGFloat = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var startPointY: CGFloat = 0.5 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var endPointX: CGFloat = 1 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var endPointY: CGFloat = 0.5 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+    
+    override func layoutSubviews() {
+        self.gradientLayer = self.layer as? CAGradientLayer
+        self.gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        self.gradientLayer.startPoint = CGPoint(x: startPointX, y: startPointY)
+        self.gradientLayer.endPoint = CGPoint(x: endPointX, y: endPointY)
+        self.layer.cornerRadius = cornerRadius
+        self.layer.shadowColor = shadowColor.cgColor
+        self.layer.shadowOffset = CGSize(width: shadowX, height: shadowY)
+        self.layer.shadowRadius = shadowBlur
+        self.layer.shadowOpacity = 1
+        
+    }
+    
+    func animate(duration: TimeInterval, newTopColor: UIColor, newBottomColor: UIColor) {
+        let fromColors = self.gradientLayer?.colors
+        let toColors: [AnyObject] = [ newTopColor.cgColor, newBottomColor.cgColor]
+        self.gradientLayer?.colors = toColors
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "colors")
+        animation.fromValue = fromColors
+        animation.toValue = toColors
+        animation.duration = duration
+        animation.isRemovedOnCompletion = true
+        animation.fillMode = .forwards
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        self.gradientLayer?.add(animation, forKey:"animateGradient")
+    }
+}
