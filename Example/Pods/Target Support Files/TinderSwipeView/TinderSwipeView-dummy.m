37c029f6ff68b71e7e7c30ef5d71b6efe7b88ddb
Nicky Patson <nickypatson@gmail.com>
2019-05-16 02:14:43 +0100

    pod update

---
+++ Example/Pods/Target Support Files/TinderSwipeView/TinderSwipeView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TinderSwipeView : NSObject
+@end
+@implementation PodsDummy_TinderSwipeView
+@end
---
+++ Example/TinderSwipeView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TinderSwipeView
+//
+//  Created by nickygriffith on 05/16/2019.
+//  Copyright (c) 2019 nickygriffith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
